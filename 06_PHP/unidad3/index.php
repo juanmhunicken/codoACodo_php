9d1315842ffac11608bb559b9f5d402699d56773
juanmhunicken <juanmhunicken@gmail.com>
2021-08-15 10:40:32 -0300

    fixed page title

--- 06_PHP/unidad3/index.php
+++ 06_PHP/unidad3/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DocumentPHP - Unidad 2</title>
+    <title>Unidad 3</title>
 
 </head>
 <body>
